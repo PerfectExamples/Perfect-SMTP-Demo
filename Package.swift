3c0b05305dd1240fe1d23a77b7758fb7bc3edb18
Rockford Wei <rocky@perfect.org>
2017-08-24 23:04:13 -0400

    Fixing lower version issue.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "Perfect-SMTP-Demo",
-    dependencies: [.Package(url:"https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 1, minor: 0)]
+    dependencies: [.Package(url:"https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 1)]
 )
